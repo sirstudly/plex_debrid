808b518ad20077c3e96576eac7b1b5ebd85280bc
sirstudly <ron1404@techie.com>
2024-07-21 01:01:19 +0100

    Fixing zilean support

--- scraper/services/zilean.py
+++ scraper/services/zilean.py
@@ -1,9 +1,11 @@
 # import modules
 from base import *
 from ui.ui_print import *
+import urllib.parse
 import releases
+import re
 
-search_url = "http://localhost:8181/dmm/search"
+base_url = "http://localhost:8181"
 name = "zilean"
 timeout_sec = 10
 session = requests.Session()
@@ -46,7 +48,7 @@ def setup(cls, new=False):
         print()
         indices = []
         for setting in settings:
-            if setting.name == "Zilean Search URL":
+            if setting.name == "Zilean Base URL":
                 setting.setup()
                 if not cls.name in active:
                     active += [cls.name]
@@ -54,38 +56,75 @@ def setup(cls, new=False):
 
 def scrape(query, altquery):
     from scraper.services import active
-    global search_url
+    ui_print("[zilean] searching for " + query + " accepting titles that regex match " + altquery)
+    global base_url
     scraped_releases = []
-    if 'zilean' in active:
-        if search_url.endswith('/'):
-            search_url = search_url[:-1]
-        try:
-            ui_print("[zilean] searching for: " + query)
-            response = session.post(search_url, headers={'Content-type': "application/json"}, data=json.dumps({'queryText': query}), timeout=timeout_sec)
+    if not 'zilean' in active:
+        return scraped_releases
 
-            if not response.status_code == 200:
-                ui_print('[zilean] error ' + str(
-                    response.status_code) + ': failed response from zilean. ' + response.content)
-                return []
+    matches_regex = altquery
+    if altquery == "(.*)":
+        matches_regex = query
+    media_type = "show" if regex.search(r'(S[0-9]|complete|S\?[0-9])', matches_regex, regex.I) else "movie"
 
-        except requests.exceptions.Timeout:
-            ui_print('[zilean] error: zilean request timed out.')
+    opts = []
+    title = query
+    if media_type == "show":
+        s = (regex.search(r'(?<=S)([0-9]+)', matches_regex, regex.I).group()
+             if regex.search(r'(?<=S)([0-9]+)', matches_regex, regex.I) else None)
+        e = (regex.search(r'(?<=E)([0-9]+)', matches_regex, regex.I).group()
+             if regex.search(r'(?<=E)([0-9]+)', matches_regex, regex.I) else None)
+        if s is not None:
+            opts.append('season=' + str(int(s)))
+            title = re.sub(r'S[0-9]+', '', title, flags=re.IGNORECASE).strip()
+        if e is not None and int(e) != 0:
+            opts.append('episode=' + str(int(e)))
+            title = re.sub(r'E[0-9]+', '', title, flags=re.IGNORECASE).strip()
+    else:
+        # find year match at the end of the query string
+        year_regex = regex.search(r'(.*)\.([12][0-9]{3})$', query, regex.I)
+        if year_regex:
+            opts.append('year=' + year_regex.group(2))
+            title = year_regex.group(1)
+
+    title = title.replace('.', ' ').replace('?', ' ').strip()
+    opts.append('query=' + urllib.parse.quote(title))
+
+    if base_url.endswith('/'):
+        base_url = base_url[:-1]
+    search_url = base_url + "/dmm/filtered?" + '&'.join(opts)
+
+    try:
+        ui_print("[zilean] using search URL: " + search_url)
+        response = session.get(search_url, timeout=timeout_sec)
+
+        if not response.status_code == 200:
+            ui_print('[zilean] error ' + str(
+                response.status_code) + ': failed response from zilean. ' + response.content)
             return []
-        except:
-            ui_print('[zilean] error: zilean couldn\'t be reached. Make sure your zilean search url [' + search_url + '] is correctly formatted.')
-            return []
 
-        try:
-            response = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
-        except:
-            ui_print('[zilean] error: unable to parse response:' + response.content)
-            return []
+    except requests.exceptions.Timeout:
+        ui_print('[zilean] error: zilean request timed out.')
+        return []
+    except:
+        ui_print(
+            '[zilean] error: zilean couldn\'t be reached. Make sure your zilean base url [' + base_url + '] is correctly formatted.')
+        return []
 
-        for result in response[:]:
-            if regex.match(r'(' + altquery.replace('.', '\.').replace("\.*", ".*") + ')', result.filename,regex.I):
-                links = ['magnet:?xt=urn:btih:' + result.infoHash + '&dn=&tr=']
-                seeders = 0  # not available
-                scraped_releases += [releases.release(
-                    '[zilean]', 'torrent', result.filename, [], float(result.filesize) / 1000000000, links, seeders)]
+    try:
+        response = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+    except:
+        ui_print('[zilean] error: unable to parse response:' + response.content)
+        return []
+
+    ui_print('[zilean] ' + str(len(response)) + ' results found.')
+    for result in response[:]:
+        if regex.match(r'(' + altquery.replace('.', '\.').replace("\.*", ".*") + ')', result.title, regex.I):
+            links = ['magnet:?xt=urn:btih:' + result.infoHash + '&dn=&tr=']
+            seeders = 0  # not available
+            scraped_releases += [releases.release(
+                '[zilean]', 'torrent', result.rawTitle, [], float(result.size) / 1000000000, links, seeders)]
+        else:
+            ui_print('[zilean] skipping ' + result.rawTitle + ' because it does not match deviation ' + altquery)
 
     return scraped_releases
